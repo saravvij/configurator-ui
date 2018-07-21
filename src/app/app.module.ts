f67d5a0b4422833b3d10430b2c849937a5132bac
Saravanan Vijayappan <saravanan.vijayappan@anaplan.com>
2018-07-21 07:47:02 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ConfiguratorComponent } from './configurator/configurator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    ConfiguratorComponent
   ],
   imports: [
     BrowserModule
